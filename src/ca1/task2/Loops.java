1aa81bae9e2ed03c3852f17145ae19ea52101ba1
kensy027 <kensy027@mymail.unisa.edu.au>
2024-04-22 06:50:43 +0930

    Implemented a For loop method for printWords and main method to dynamically get input from user on how many times to print words

--- src/ca1/task2/Loops.java
+++ src/ca1/task2/Loops.java
@@ -1,7 +1,34 @@
 package ca1.task2;
 
+import java.util.Scanner;
+
 public class Loops {
-	public void printWords(int numberOfTimes) {
-		
-	}
+
+    /**
+     * Prints the word "words" the specified number of times.
+     *
+     * @param numberOfTimes the number of times to print "words".
+     */
+    public void printWords(int numberOfTimes) {
+        for (int i = 0; i < numberOfTimes; i++) {
+            System.out.println("words");
+        }
+    }
+
+    /**
+     * Main method to test the printWords method.
+     * Reads the number of times to print from the user.
+     *
+     * @param args command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter the number of times to print 'words': ");
+        int numberOfTimes = scanner.nextInt();
+
+        Loops loops = new Loops();
+        loops.printWords(numberOfTimes); // Call the method with user input
+
+        scanner.close();
+    }
 }
