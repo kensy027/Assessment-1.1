d6f5ea92ed5b3e76b7d0a34d519ee0c33bb61504
kensy027 <kensy027@mymail.unisa.edu.au>
2024-04-22 18:00:05 +0930

    Finalised code v1.0 and added documentations

--- src/ca1/task4/Car.java
+++ src/ca1/task4/Car.java
@@ -6,12 +6,28 @@ public class Car {
     private String model;
     private String color;
     
+	/**
+     * Constructs a new Car instance.
+     *
+     * @param make  The make of the car (e.g., Honda).
+     * @param model The model of the car (e.g., Civic).
+     * @param color The color of the car (e.g., Red).
+     */
+
     public Car(String make, String model, String color) {
         this.make = make;
         this.model = model;
         this.color = color;
     }
 
+	/**
+     * Checks if this Car is equal to another object.
+     * If the other object is a Car and has the same model and color,
+     * then the two cars are considered equal.
+     *
+     * @param o The object to compare this Car against.
+     * @return true if the given object represents a Car equivalent to this car, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -19,12 +35,23 @@ public class Car {
         Car car = (Car) o;
         return Objects.equals(model, car.model) && Objects.equals(color, car.color);
     }
-
+	/**
+     * Generates a hash code for this Car.
+     * The hash code is generated using the model and color attributes.
+     *
+     * @return A hash code value for this Car.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(model, color);
     }
-
+	
+	/**
+     * Returns a string representation of this Car.
+     * The string is formatted as "make, model, color".
+     *
+     * @return A string representation of this Car.
+     */
     @Override
     public String toString() {
         return make + ", " + model + ", " + color;
--- src/ca1/task4/CarProcessing.java
+++ src/ca1/task4/CarProcessing.java
@@ -25,9 +25,15 @@ public class CarProcessing {
 			System.out.println(c);
 		}
 	}
-	
+	/* The method uses a HashSet to maintain a collection of unique Car objects. Since the Car class
+	* has overridden equals and hashCode to consider only model and color for comparison,
+	* the HashSet will use these methods to prevent adding duplicate Car objects.
+	*
+	* @param inputArray The array of Car objects that may contain duplicates.
+	* @return A new array of Car objects with duplicates removed.
+	*/
 	public Car[] removeDuplicates(Car[] inputArray) {
-        Set<Car> carSet = new HashSet<>(Arrays.asList(inputArray));
-        return carSet.toArray(new Car[0]);
+        Set<Car> carSet = new HashSet<>(Arrays.asList(inputArray)); // Create a HashSet from the array of Car objects. Duplicates are identified by the model and color of the Car objects and automatically removed.
+        return carSet.toArray(new Car[0]);// Convert the HashSet back to an array of Car objects and return it. The new array will not contain any duplicate Car objects.
     }
 }
